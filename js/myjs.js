5e379d82c80011d9175f279e54f6c4c6dce93b80
ZephV <zephkwok@outlook.com>
2017-12-02 23:49:09 +0800

    add jquery.js and myjs.js

---
+++ js/myjs.js
@@ -0,0 +1,8 @@
+// $(function(){
+// 	var btn = $('#btn_test');
+// 	btn.click(function(){
+// 		var b =$('#presonal_info');
+// 		b.animation({margin-left:-200px;});
+// 		console.log("ni好");
+// 	});
+// })
