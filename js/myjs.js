1b246928cb343b18c795eca45ed5f3e6d24d6760
ZephV <zephkwok@outlook.com>
2017-12-06 23:57:03 +0800

    and h1

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,9 @@
 $(function(){
 
-	// /* */
+	
 	console.log("%c 由于时间原因网页没有做到最好的优化,代码写的不好的地方请多多包涵!需要一份前端的工作,如果贵公司有我合适的职位请联系我","color:yellow");
 	console.log("邮箱: %c 309516155@qq.com","color:red;font-size:14px");
 	
-
 
 	var wellcome_page = $('#wellcome_page');
 	var enter_btn = $('#enter_btn');
@@ -29,6 +28,18 @@ $(function(){
 			$("#the_rightbox").animate({marginTop:"30px"},2000);
 		});
 	})
+
+	$(document).on("mousewheel DOMMouseScroll", function (e) {
+	    var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
+	                (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));              // firefox
+	    if (delta > 0) {
+	        // 向上滚
+	        console.log("wheelup");
+	    } else if (delta < 0) {
+	        // 向下滚
+	        console.log("wheeldown");
+	    }
+	});
 
 	
 })
