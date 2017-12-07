e4991cf3723348b508488e4095e0df016c7d64ae
ZephV <zephkwok@outlook.com>
2017-12-07 22:26:46 +0800

    change css

--- js/myjs.js
+++ js/myjs.js
@@ -11,35 +11,58 @@ $(function(){
 	var wellcome_page_wrap = $('.wellcome_page_wrap');
 	var div_height = wellcome_page_wrap.outerHeight();
 	var window_height = $(window).height();
-	var wellcome_page_height = (window_height/2)-(div_height/2);
+	console.log(wellcome_page_wrap);
+	var wellcome_page_height = (window_height/3)-(div_height/2);
 
 	wellcome_page_wrap.css('marginTop',wellcome_page_height);
 
 	setTimeout(function(){
-		wellcome_page.fadeIn(1000);
-		log.fadeIn(3000);
-		enter_btn.slideDown(2000);
+		wellcome_page.fadeIn(50);//1000
+		log.fadeIn(50);//3000
+		enter_btn.slideDown(50);//2000
 	},100);
 
 
 	enter_btn.click(function(){
-		wellcome_page_wrap.fadeOut(2000,function(){
-			$("#presonal_info").animate({marginLeft:"25px"},2000);
-			$("#the_rightbox").animate({marginTop:"30px"},2000);
+		wellcome_page_wrap.fadeOut(50,function(){//2K - -
+			$("#presonal_info").animate({marginLeft:"25px"},50);
+			$("#the_rightbox").animate({marginTop:"30px"},50);
 		});
-	})
-
-	$(document).on("mousewheel DOMMouseScroll", function (e) {
-	    var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
-	                (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));              // firefox
-	    if (delta > 0) {
-	        // 向上滚
-	        console.log("wheelup");
-	    } else if (delta < 0) {
-	        // 向下滚
-	        console.log("wheeldown");
-	    }
 	});
 
+	$('.my_resume').hover(function(){
+		$('.my_resume').animate({width:'300px'},"slow");
+		$('.my_resume').animate({height:'100px'},"slow");
+		console.log("aaa");
+	});
+
+	// overflow 隐藏后滚动 任然可以滚动
+	// $(document).on("mousewheel DOMMouseScroll", function (e) {
+	//     var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
+	//                 (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));              // firefox
+	//     if (delta > 0) {
+	//         // 向上滚
+	//         console.log(window.pageYOffset);
+	//     } else if (delta < 0) {
+	//         // 向下滚
+	//         console.log("wheeldown");
+	//     }
+	// });
+
+
+	// 自我介绍弹窗
+	// $('.abme_click').on('click',function(){
+	// 	var msg = '我叫郭锐,是个活泼开朗的男生.什么?谁说活泼开朗就不能当码农的?哼!当然我在学习和码代码的时候是非常安静和专注的.我的爱好是动漫;游戏;学习.对没错就是学习!最近越来越喜欢学习(真的后悔迷茫混日子没好好读高中,落到个大专的下场,现在简历投出去就跟沉入海了一样一点回声都没~).'
+ //                    +'生活中,我是一个和别人相处比较融洽的人,善于倾听别人的话,喜欢团队合作.如果你通过这个网页了解我,觉得我还可以请一定要联系我给我一个面试的机会或工作的机会哦,感谢你读完我这一大段唠叨.'
+	// 	layer.open({
+	// 		type:1,
+	// 		shadeClose:true,
+	// 		area:['600px','360px'],
+	// 		content: '\<\div style="padding:20px;">'+msg+'\<\/div>'
+
+	// 	})
+	// })
 	
+
 })
+
